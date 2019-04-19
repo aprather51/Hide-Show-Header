8fcc35d16e2f08c8314a6e8f0c64fe5093e2afe5
Arthur Prather <apratherjr@gmail.com>
2019-04-19 09:07:46 -0500

    Updates

---
+++ precache-manifest.86387202078a880546c32c52c1c017a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5afb4963a7b520c599",
+    "url": "/Hide-Show-Header/static/js/runtime~main.9f5afb49.js"
+  },
+  {
+    "revision": "47e1ff84522f74f8744f",
+    "url": "/Hide-Show-Header/static/js/main.47e1ff84.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/Hide-Show-Header/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "47e1ff84522f74f8744f",
+    "url": "/Hide-Show-Header/static/css/main.497b3508.chunk.css"
+  },
+  {
+    "revision": "2800f77ad94f3468f4e5e7fe629aaf13",
+    "url": "/Hide-Show-Header/index.html"
+  }
+];
