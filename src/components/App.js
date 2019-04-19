0d79baa935ef586aae82b17967486de707c926c5
Arthur Prather <apratherjr@gmail.com>
2019-04-19 09:04:10 -0500

    switch component to stateless

--- src/components/App.js
+++ src/components/App.js
@@ -1,98 +1,60 @@
-import React, { Component, Fragment } from 'react';
-import TopBar from './TopBar';
+import React, { Fragment } from 'react';
+import Header from './Header';
 import './app.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <section>
-          <TopBar />
-          <h1>Title</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-            exercitationem nobis quas fugiat eaque reprehenderit rerum quo,
-            consectetur quis sapiente odit voluptatibus id molestias doloremque
-            totam pariatur laborum atque repellat reiciendis aliquid quos
-            accusantium eum! Quidem, possimus inventore soluta enim odit aperiam
-            sequi et ducimus neque, vero architecto itaque iure!
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ullam
-            omnis atque adipisci suscipit et molestias, distinctio libero
-            deserunt nobis quidem veritatis perferendis, dolores ducimus
-            reprehenderit quas nesciunt nisi ex dignissimos eum neque minus.
-            Quae quia molestiae sed ipsum dignissimos.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
-            quibusdam dolorem quos sunt accusamus illo debitis nihil
-            accusantium. Quod labore eos unde accusamus aut porro nobis
-            aspernatur? Aut hic accusantium corrupti quisquam doloribus! Labore
-            expedita nulla obcaecati unde soluta animi facere ea, laboriosam
-            veritatis nostrum eos incidunt porro suscipit delectus
-            exercitationem dolorum harum enim vero omnis amet quisquam inventore
-            maiores adipisci illo? Incidunt cupiditate quas consequuntur
-            blanditiis provident eum fugit.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fuga
-            harum, dolore consequatur praesentium aliquid impedit hic
-            reprehenderit earum deserunt, aperiam dicta. Porro quae fuga
-            molestias minus consectetur commodi reprehenderit laudantium illo
-            nisi tenetur ab maiores autem eaque totam, nesciunt dolor vitae iste
-            officiis asperiores.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ullam
-            omnis atque adipisci suscipit et molestias, distinctio libero
-            deserunt nobis quidem veritatis perferendis, dolores ducimus
-            reprehenderit quas nesciunt nisi ex dignissimos eum neque minus.
-            Quae quia molestiae sed ipsum dignissimos.
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-            neque doloremque, velit perferendis, soluta asperiores, quidem
-            maiores magni facere ea fugiat? Autem ex corrupti ratione!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nobis
-            reiciendis iusto quibusdam voluptatum laudantium excepturi velit
-            nisi mollitia tempore ex, animi iste ab officia voluptatibus impedit
-            saepe corporis exercitationem officiis quia voluptatem. Accusantium
-            sequi voluptatum omnis facere aliquam velit nulla illo provident
-            maiores, ab voluptates, veniam consequatur obcaecati sint quas
-            impedit ducimus molestias hic eum eligendi corporis suscipit
-            tempore. Eligendi earum sed necessitatibus amet qui autem cum ea
-            omnis dolores assumenda? Iusto voluptatem animi atque consequatur
-            quisquam maxime quasi magni pariatur inventore rerum perspiciatis
-            ullam officiis quibusdam soluta, sequi reiciendis distinctio tenetur
-            neque eius modi culpa asperiores! Consectetur, inventore?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fuga
-            harum, dolore consequatur praesentium aliquid impedit hic
-            reprehenderit earum deserunt, aperiam dicta. Porro quae fuga
-            molestias minus consectetur commodi reprehenderit laudantium illo
-            nisi tenetur ab maiores autem eaque totam, nesciunt dolor vitae iste
-            officiis asperiores.
-          </p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ullam
-            omnis atque adipisci suscipit et molestias, distinctio libero
-            deserunt nobis quidem veritatis perferendis, dolores ducimus
-            reprehenderit quas nesciunt nisi ex dignissimos eum neque minus.
-            Quae quia molestiae sed ipsum dignissimos.
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
-            neque doloremque, velit perferendis, soluta asperiores, quidem
-            maiores magni facere ea fugiat? Autem ex corrupti ratione!
-          </p>
-        </section>
-      </Fragment>
-    );
-  }
-}
+const App = () => (
+	<Fragment>
+		<section>
+			<Header />
+			<h1>Hello</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quae odio
+				minus. Mollitia, sunt atque, quam illum, eum adipisci eveniet beatae ut
+				illo accusamus laudantium dolor similique! Aliquam, similique, magnam
+				nihil ex possimus labore tempora eligendi quibusdam dicta eveniet quae
+				harum fugiat quam maiores? Reiciendis dolores officia obcaecati, nulla
+				tempore perspiciatis iure, provident praesentium nisi rem doloribus
+				beatae ut ex nesciunt incidunt molestias fugiat assumenda debitis,
+				facilis quisquam ea dolorum corrupti quod optio! Sint repellat
+				aspernatur hic. Debitis veniam et vel, laboriosam cum quo hic quibusdam
+				adipisci necessitatibus voluptas harum corporis nesciunt magnam nemo
+				iusto! Veniam distinctio vero rem, magnam voluptatibus temporibus
+				quisquam, recusandae commodi, iste animi nostrum assumenda? Consectetur
+				dicta iure beatae sunt excepturi earum adipisci sit atque ipsa
+				explicabo. Ut doloribus voluptatum asperiores veritatis officiis quos
+				quidem. Molestiae mollitia consequuntur aliquam quisquam libero cumque
+				accusamus provident delectus? Impedit corporis dolores necessitatibus
+				nobis minus adipisci quae soluta optio distinctio.
+			</p>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+				consectetur voluptatibus. Sint, quidem. Doloribus cum quis vero
+				quibusdam eos facere! Laboriosam cupiditate hic, facilis sint inventore
+				deleniti eligendi corrupti, eum odio voluptatem delectus minus eaque
+				iusto molestiae maiores repellendus minima cumque numquam dignissimos
+				amet fugit? Corrupti quo expedita amet voluptatum?
+			</p>
+			<img
+				src="https://images.pexels.com/photos/987586/pexels-photo-987586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+				alt="netflix"
+			/>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+				corrupti fugiat veritatis numquam. Illo numquam, quasi alias et maiores
+				voluptatum beatae. Rerum nisi rem fugiat saepe dolores vitae doloribus
+				fugit minima asperiores accusantium nobis incidunt voluptate quia veniam
+				iste eveniet harum quaerat ad, hic cupiditate ipsam. Porro esse non
+				praesentium perferendis eligendi! Temporibus soluta officia blanditiis
+				earum magni? Quibusdam harum deleniti iusto delectus dolores quam rem
+				quod nobis debitis sapiente sint, tempora itaque? Officia doloremque
+				quis officiis blanditiis quae nemo quo accusamus quidem, eaque provident
+				aliquid vero quasi itaque dicta voluptas, esse dolor. Enim nam amet iure
+				voluptate nemo eligendi nobis odio dolorum eum id consequatur aliquid
+				ipsam voluptatibus sequi repudiandae dolor dolores ex ab ut et
+				laudantium, rem ducimus!
+			</p>
+		</section>
+	</Fragment>
+);
 
 export default App;
